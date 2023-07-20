ba078f33cef68d5ab6a091cfd2875bd3d06d5684
sh-saha <shourovsaha94@gmail.com>
2023-07-20 19:04:00 +0600

    added pagination in product page.

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -7,21 +7,44 @@ import { ProductRepository } from "../model/products.repositiry";
     templateUrl: "store.component.html"
 })
 
-export class StoreComponent{
+export class StoreComponent {
 
     public selectedCategory: string = null;
+    public productPerPage: number = 5;
+    public selectedPageIndex: number = 1;
 
-    constructor(private productsRepo: ProductRepository){}
+    constructor(private productsRepo: ProductRepository) { }
 
-    get products(): Product[]{
-        return this.productsRepo.GetProductsByCategory(this.selectedCategory);
+    get products(): Product[] {
+        let pageIndex = (this.selectedPageIndex - 1) * this.productPerPage;
+
+        let a = this.productsRepo.GetProductsByCategory(this.selectedCategory)
+        .slice(pageIndex, pageIndex + this.productPerPage);
+
+        return this.productsRepo.GetProductsByCategory(this.selectedCategory)
+            .slice(pageIndex, pageIndex + this.productPerPage);
     }
 
-    get categories(): string[]{
+    get categories(): string[] {
         return this.productsRepo.GetCategories();
     }
 
-    changeCategory(newCategory?: string){
+    changeCategory(newCategory?: string) {
         this.selectedCategory = newCategory;
+    }
+
+    changePage(newPageIndex: number) {
+        this.selectedPageIndex = newPageIndex;
+    }
+
+    changePageSize(event: Event){
+        this.productPerPage = Number((event.target as HTMLInputElement).value);
+        this.changePage(1);
+    }
+
+    get pageNumbers(): number[]{
+        return Array(Math.ceil(this.productsRepo.GetProductsByCategory(this.selectedCategory).length / this.productPerPage))
+                    .fill(0)
+                    .map((x, index) => index + 1);               
     }
 }
