d278160e46941ce5322cd2b1632e28f35eabcbc6
sh-saha <shourovsaha94@gmail.com>
2023-07-25 12:36:42 +0600

    added REST Api datasource.

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -4,8 +4,11 @@ import { ProductRepository } from "./products.repositiry";
 import { StaticDataSource } from "./static.datasource";
 import { Order } from "../model/order.model";
 import { OrderRepository } from "../model/order.repository";
+import { HttpClientModule } from "@angular/common/http";
+import { RestDataSource } from "./rest.datasource";
 
 @NgModule({
-    providers: [ProductRepository, StaticDataSource, Cart, Order, OrderRepository]
+    imports: [HttpClientModule],
+    providers: [ProductRepository, Cart, Order, OrderRepository, {provide: StaticDataSource, useClass: RestDataSource}]
 })
 export class ModelModule{}
